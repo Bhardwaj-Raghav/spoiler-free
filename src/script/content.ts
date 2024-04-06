fe21b019d7184baa891c1c7f2f48fdbb4183335b
Raghav Bhardwaj <r001dbhardwaj@gmail.com>
2024-04-06 00:29:00 -0700

    added title page video block feature.

--- src/script/content.ts
+++ src/script/content.ts
@@ -1,43 +1,62 @@
 'use strict';
 
-
-// function createRegexFromWords(words) {
-//     // Escape special characters in each word and join them with '|' to create a regex pattern
-//     const regexPattern = words.map(word => word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|');
-//     // Create a RegExp object with the constructed pattern and 'i' flag for case-insensitive matching
-//     return new RegExp(regexPattern, 'i');
-// }
-
-
-chrome.storage.local.get().then((test) => {
-    console.log({ test });
-
-});
-
-const doesMatchKeyWords = (string) => {
-    return /(Spider man)|(Spiderman)|(Spider-man)|(Spider-man 2)|(Spiderman 2)|(Peter)|(Parker)|(Miles)|(morales)|(leak)|(venom)/gi.test(
-        string
-    );
+let keywordRegex: RegExp;
+let _settings = {
+    title: true,
+    search: true,
+    shorts: true,
+    channel: true,
 };
 
-
-
-const mutationObserver = (mutation, observer) => {
-
-    document.querySelectorAll("yt-formatted-string.style-scope.ytd-rich-grid-media").forEach((element) => {
-
-        // console.log("Test", element);
-
-    })
-
-
-    setTimeout(() => {
-        observer.disconnect();
-    }, 5000);
+function createRegexFromWords(words: string[]) {
+    // Escape special characters in each word and join them with '|' to create a regex pattern
+    const regexPattern = '\\b' + words.map(word => word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('\\b|\\b') + '\\b';
+    // Create a RegExp object with the constructed pattern and 'i' flag for case-insensitive matching
+    return new RegExp(regexPattern, 'i');
 }
 
-const domLayoutObserver = new MutationObserver(mutationObserver);
-domLayoutObserver.observe(document.body, {
-    childList: true,
-    subtree: true
+
+chrome.storage.local.get().then(({ keywords, settings }) => {
+    if (keywords.keywords) {
+        keywordRegex = createRegexFromWords(keywords.keywords);
+        initFunctions();
+    }
+    if (settings) {
+        _settings.title = settings.title;
+        _settings.search = settings.search;
+        _settings.shorts = settings.shorts;
+        _settings.channel = settings.channel;
+    }
+    console.log(keywords, keywordRegex);
+
 });
+
+const initFunctions = () => {
+
+
+    const mutationObserver = (mutation, observer) => {
+        requestAnimationFrame(() => {
+            if (_settings.title) {
+                document.querySelectorAll("yt-formatted-string.style-scope.ytd-rich-grid-media#video-title").forEach((element: HTMLElement) => {
+                    const title = element.innerText;
+                    if (doesMatchKeyWords(title)) {
+                        console.log(title);
+                        element.closest("ytd-rich-item-renderer.style-scope").remove();
+                        // Update session list for removed videos (may be)
+                    }
+
+                });
+            }
+        });
+    }
+    const domLayoutObserver = new MutationObserver(mutationObserver);
+    domLayoutObserver.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+
+}
+
+const doesMatchKeyWords = (string) => {
+    return keywordRegex.test(string);
+};
