4199732d8b4c71a7ec9591e2f428e7368431811c
Raghav Bhardwaj <r001dbhardwaj@gmail.com>
2024-04-06 23:53:30 -0700

    added functions to remove videos and shorts based on keywords

--- src/script/content.ts
+++ src/script/content.ts
@@ -27,42 +27,58 @@ chrome.storage.local.get().then(({ keywords, settings }) => {
     }
     if (keywords && keywords.length) {
         keywordRegex = createRegexFromWords(keywords);
-        const page = currentPage();
-        if (
-            (page === "title" && _settings.title) ||
-            (page === "subscription" && _settings.subscription) ||
-            (page === "search" && _settings.search) ||
-            (page === "shorts" && _settings.shorts) ||
-            (page === "channel" && _settings.channel)
-        ) {
-            initFunctions();
-        }
+        initFunctions();
     }
     console.log(keywords, keywordRegex);
 });
 
 const initFunctions = () => {
     const mutationObserver = (mutation, observer) => {
+        const page = currentPage();
         requestAnimationFrame(() => {
-            if (_settings.title) {
-                let newTitles = false;
-                for (let i = 0; i < mutation.length; i++) {
-                    if ('video-title' == mutation[i].target.id && 'style-scope ytd-rich-grid-media' == mutation[i].target.className) {
-                        newTitles = true;
-                        console.log("newTitles");
-                        break;
-                    }
-                }
-                if (newTitles) {
-                    document.querySelectorAll("yt-formatted-string.style-scope.ytd-rich-grid-media#video-title").forEach((element: HTMLElement) => {
-                        const title = element.innerText;
-                        if (doesMatchKeyWords(title)) {
-                            console.log(title);
-                            element.closest("ytd-rich-item-renderer.style-scope").remove();
-                            // Update session list for removed videos (may be)
-                        }
-                    });
-                }
+            if (page === "title" && _settings.title) {
+                runRemoveFunc(
+                    mutation,
+                    'yt-formatted-string',
+                    'video-title',
+                    'style-scope ytd-rich-grid-media',
+                    [
+                        "yt-formatted-string.style-scope.ytd-rich-grid-media#video-title",
+                        "#video-title.style-scope.ytd-rich-grid-slim-media"
+                    ], [
+                    "ytd-rich-item-renderer.style-scope",
+                    "ytd-rich-item-renderer.style-scope"
+                ]);
+            } else if (page === "subscription" && _settings.subscription) {
+                runRemoveFunc(
+                    mutation,
+                    'yt-formatted-string',
+                    'video-title',
+                    'style-scope ytd-rich-grid-media',
+                    [
+                        "yt-formatted-string.style-scope.ytd-rich-grid-media#video-title",
+                        "#video-title.style-scope.ytd-rich-grid-slim-media"
+                    ], [
+                    "ytd-rich-item-renderer.style-scope",
+                    "ytd-rich-item-renderer.style-scope"
+                ]);
+            } else if (page === "search" && _settings.search) {
+                runRemoveFunc(
+                    mutation,
+                    'yt-formatted-string',
+                    '',
+                    'style-scope ytd-video-renderer',
+                    [
+                        "yt-formatted-string.style-scope.ytd-video-renderer",
+                        "#video-title.style-scope.ytd-reel-item-renderer"
+                    ], [
+                    ".style-scope.ytd-item-section-renderer",
+                    "ytd-reel-item-renderer.style-scope.yt-horizontal-list-renderer"
+                ]);
+            } else if (page === "channel" && _settings.channel) {
+                runSubscriptionPageFunc();
+            } else if (page === "shorts" && _settings.shorts) {
+                runShortRemoveFunc();
             }
         });
     }
@@ -74,10 +90,81 @@ const initFunctions = () => {
     });
 
 }
+// Not Optimized
+const runSubscriptionPageFunc = () => {
+    document.querySelectorAll(".yt-simple-endpoint.style-scope.ytd-grid-video-renderer").forEach((element: HTMLElement) => {
+        if (doesMatchKeyWords(element.innerText)) {
+            element.closest("ytd-grid-video-renderer.style-scope.yt-horizontal-list-renderer").remove();
+            // Update session list for removed videos (may be)
+        }
+    });
+    document.querySelectorAll(".style-scope.ytd-reel-item-renderer").forEach((element: HTMLElement) => {
+        if (doesMatchKeyWords(element.innerText)) {
+            element.closest("ytd-reel-item-renderer.style-scope.yt-horizontal-list-renderer").remove();
+            // Update session list for removed videos (may be)
+        }
+    });
+    document.querySelectorAll("yt-formatted-string.style-scope.ytd-rich-grid-media").forEach((element: HTMLElement) => {
+        if (doesMatchKeyWords(element.innerText)) {
+            element.closest("ytd-rich-item-renderer.style-scope.ytd-rich-grid-row").remove();
+            // Update session list for removed videos (may be)
+        }
+    });
+    document.querySelectorAll(".style-scope.ytd-rich-grid-slim-media#video-title").forEach((element: HTMLElement) => {
+        if (doesMatchKeyWords(element.innerText)) {
+            element.closest("ytd-rich-item-renderer.style-scope.ytd-rich-grid-row").remove();
+            // Update session list for removed videos (may be)
+        }
+    });
+}
+const runShortRemoveFunc = () => {
+    document.querySelectorAll("ytd-reel-video-renderer[is-active].reel-video-in-sequence.style-scope.ytd-shorts yt-formatted-string.style-scope.reel-player-header-renderer").forEach((element: HTMLElement) => {
+        if (doesMatchKeyWords(element.innerText)) {
+            element.closest("ytd-reel-video-renderer[is-active].reel-video-in-sequence.style-scope.ytd-shorts").remove();
+            // Update session list for removed videos (may be)
+        }
+    });
+}
+
+const runRemoveFunc = (mutation, tagname: string, id: string, className: string, querySelecter: string[], elementToDeleteSelector: string[]) => {
+    let newTitles = false;
+    for (let i = 0; i < mutation.length; i++) {
+        console.log({
+            Value: id == mutation[i].target.id &&
+                className == mutation[i].target.className &&
+                tagname == mutation[i].target.tagName.toLowerCase(),
+            id,
+            mId: mutation[i].target.id,
+            className,
+            mClassName: mutation[i].target.className,
+            tagname,
+            mTagname: mutation[i].target.tagName.toLowerCase()
+        });
+
+        if (
+            id == mutation[i].target.id &&
+            className == mutation[i].target.className &&
+            tagname == mutation[i].target.tagName.toLowerCase()
+        ) {
+            newTitles = true;
+            break;
+        }
+    }
+    if (newTitles) {
+        console.log("newTitles");
+        for (let i = 0; i < querySelecter.length; i++) {
+            document.querySelectorAll(querySelecter[i]).forEach((element: HTMLElement) => {
+                if (doesMatchKeyWords(element.innerText)) {
+                    element.closest(elementToDeleteSelector[i]).remove();
+                    // Update session list for removed videos (may be)
+                }
+            });
+        }
+    }
+}
 
 const currentPage = (): string => {
     const currentPath: string = window.location.pathname;
-    console.log(currentPath);
     if (currentPath === "/") {
         return "title";
     } else if (currentPath === "/results") {
